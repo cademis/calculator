eb0843bbad4f0b1dddc4810d91667ca63c821178
chris <caitken86@gmail.com>
2023-06-05 06:05:04 +0100

    create the css and add some logic

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 const display = document.querySelector(".display");
 
-display.innerHTML = "Calcullator goes here";
+display.innerText = 0;
 
-const add = (a, b) => a + b;
+const keys = document.querySelector(".keys").querySelectorAll("button");
 
-const subtract = (a, b) => a - b;
+let cumulativeValue = 0;
 
-const multiply = (a, b) => a * b;
+const calculation = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => (b === 0 ? "Error: Division by zero" : a / b),
+};
 
-const divide = (a, b) => a / b;
+// handle click
+const keyPress = () => {
+  for (let key of keys) {
+    key.addEventListener("click", () => {
+      // check if not a number or decimal
+      const regex = /[\/\*\-\+=\.]/;
+      if (regex.test(key.innerText)) {
+        let currentValue = +display.innerText;
+        let operator = key.innerText;
+        console.log(operator);
+        const result = calculation[operator](cumulativeValue, currentValue);
+        display.innerText = 0;
+        // if a decimal point and a decimal exists on the displat, do nothing
+      } else if (key.innerText === "." && display.innerText.includes(".")) {
+        console.log("do nothing");
+
+        // if anything else, concat the number to the display
+      } else {
+        display.innerText += key.innerText;
+        display.innerText = +display.innerText;
+      }
+    });
+  }
+};
+
+keyPress();
